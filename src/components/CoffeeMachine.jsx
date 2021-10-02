d0f71bc3b12ff7a337e83560a4e790ba3a3601be
Sam <samwbru@gmail.com>
2021-10-02 11:42:45 +0200

    Refactor of Drink state mechanism
    - Refactored Drink component into a class. Updated the state handling mechanism.
    - Updated the drinks data. Now the drinks data stored in an array obj in the Drink class.
    - fixed image references
    
    To Do:
    - when adding a drink for first time, setting the state variables such as date added and drink name do not save. only when clicking add for the second time does this data get stored/sent.

--- src/components/CoffeeMachine.jsx
+++ src/components/CoffeeMachine.jsx
@@ -1,8 +1,30 @@
 import '../Coffeemachine.css';
 import Drink from "./Drink";
 import cafeLatteImg from "../img/cafe-latte.png";
+import mochaImg from "../img/mochaccino.png";
+import cafeTeImg from "../img/cafe-te.png";
+import miamiViceImg from "../img/miami-vice.png";
+import { useState } from "react";
 
 function CoffeeMachine(){
+    const anOrder = {
+        id: 0,
+        drinks: [{}]
+    }
+    const [order, setOrder] = useState(anOrder);
+
+    function addDrinkToOrder(drink){
+        setOrder(prevOrder => {
+            // let {id} = drink; // get the drink id
+
+            return{
+                ...prevOrder,
+                drinks: [...prevOrder.drinks, drink]
+            }
+            
+        })
+    }
+
     return(
         <div className="coffeeMachine">
             <hgroup className="machineHeader">
@@ -13,31 +35,39 @@ function CoffeeMachine(){
             <section className="machineBody">
                 <Drink 
                 key="1"
+                id="1"
                 itemName="Café Latte" 
                 description="A drink you will love" 
                 imgSrc={cafeLatteImg}
-                imgAlt="Café latte" 
+                imgAlt="Cafe latte" 
+                addDrinkToOrder={addDrinkToOrder}
                 />
                 <Drink 
                 key="2"
+                id="2"
                 itemName="Mocha" 
                 description="Get that chocolate feel" 
-                imgSrc="../img/mocha.png" 
-                imgAlt="Mocha"
+                imgSrc={mochaImg} 
+                imgAlt="Mocha cafe"
+                addDrinkToOrder={addDrinkToOrder}
                 />
                 <Drink 
                 key="3"
+                id="3"
                 itemName="Café Té" 
                 description="Enjoy coffee & tea together" 
-                imgSrc="../img/cafe-te.png"
-                imgAlt="Café Té"
+                imgSrc={cafeTeImg}
+                imgAlt="Cafe tea"
+                addDrinkToOrder={addDrinkToOrder}
                 />
                 <Drink 
                 key="4"
+                id="4"
                 itemName="Miami Vice" 
                 description="Feel the Miami vibe" 
-                imgSrc="../img/miami-vice-coffee.png" 
-                imgAlt="Miami Vice" 
+                imgSrc={miamiViceImg} 
+                imgAlt="Miami Vice coffee" 
+                addDrinkToOrder={addDrinkToOrder}
                 />
             </section>
         </div>
--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -1,33 +1,98 @@
-import { useState } from "react";
+import { Component } from "react";
 
+const drinks = [
+    {
+        id: 1,
+        name: "Café Latte",
+        desc: "A drink you will love. Traditional Coffee with milk.",
+        imgSrc: "../img/cafe-latte.png",
+        imgAlt: "Café latte drink",
+        price: 1.50
+    },
+    {
+        id: 2,
+        name: "Café Té",
+        desc: "Enjoy coffee & tea together",
+        imgSrc: "../img/cafe-te.png",
+        imgAlt: "Café Té drink",
+        price: 2
+    },
+    {
+        id: 3,
+        name: "Mochacoco",
+        desc: "Get that chocolate feel",
+        imgSrc: "../img/mocha.png",
+        imgAlt: "Mochacoco drink",
+        price: 2.50
+    },
+    {
+        id: 3,
+        name: "Mochacoco",
+        desc: "Get that chocolate feel",
+        imgSrc: "../img/mocha.png",
+        imgAlt: "Mochacoco drink",
+        price: 2.50
+    }
+];
 
-function Drink(props){
-    const [quantity, setQuantity] = useState(0);
-
-    function minusQuantity(){
-        setQuantity(quantity -1);
+export default class Drink extends Component{
+    state = {
+        dateAdded: 0,
+        drinkName: 0,
+        quantity: 0,
+        id: 0
     }
 
-    function addQuantity(){
-        setQuantity( quantity + 1 );
+    addQuantity = () => {
+        this.setState({
+            id: this.props.id,
+            quantity: this.state.quantity+1
+        })
     }
 
-    return(
-        <div className="machineItem">
-            <hgroup>
-                <h2>{props.itemName}</h2>
-                <h3>{props.description}</h3>
-            </hgroup>
-            
-            <div className="itemBody">
-                <button type="button" onClick={minusQuantity}>-</button>
-                <img src={props.imgSrc} alt={props.imgAlt} />
-                <button type="button" onClick={addQuantity}>+</button>
+    minusQuantity = () => {
+        if( this.state.quantity > 0 ){
+            this.setState({
+                id: this.props.id,
+                quantity: this.state.quantity-1
+            })
+        }
+    }
+
+    getQuantity = () =>{
+        return this.state.quantity;
+    }
+
+    getDrink = () =>{
+        return this.state;
+    }
+
+    addDrink = () =>{
+        this.setState({
+            dateAdded: new Date().toLocaleTimeString(), // this info is not persisting the first time round.
+            drinkName: this.props.itemName
+        });
+        console.log( this.getDrink() );
+        this.props.addDrinkToOrder( this.getDrink() );
+    }
+
+    render() {
+        return(
+            <div className="machineItem">
+                <hgroup>
+                    <h2>{this.props.itemName}</h2>
+                    <h3>{this.props.description}</h3>
+                </hgroup>
+                
+                <div className="itemBody">
+                    <button type="button" onClick={this.minusQuantity}>-</button>
+                    <img src={this.props.imgSrc} alt={this.props.imgAlt} />
+                    <button type="button" onClick={this.addQuantity}>+</button>
+                </div>
+                <p className="itemQty">Qty: {this.getQuantity()}</p>
+                <button type="button" onClick={this.addDrink}>add</button>
+                
             </div>
-            <p className="itemQty">Qty: {quantity}</p>
-            <button type="button">add</button>
-        </div>
-    )
+        )
+    }
 }
-
-export default Drink;
